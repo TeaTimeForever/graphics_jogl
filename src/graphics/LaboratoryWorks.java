7ff5b1c6744af12c1d4ba68f1131fe84fb1db924
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2014-03-09 18:17:17 +0200

    rotation, scaling and transfering

--- src/graphics/LaboratoryWorks.java
+++ src/graphics/LaboratoryWorks.java
@@ -1,8 +1,11 @@
 package graphics;
+import java.awt.Frame;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import javax.media.opengl.GL;
@@ -15,47 +18,101 @@ import javax.media.opengl.awt.GLCanvas;
 import javax.media.opengl.glu.GLU;
 
 public class LaboratoryWorks {
-
+	private static Map<TransformationType, Float> map;
+	
+	private static void initMap() {
+		map = new HashMap<TransformationType, Float>();
+		map.put(TransformationType.ROTATE_C, 0f);
+		map.put(TransformationType.ROTATE_Z, 0f);
+		map.put(TransformationType.SCALE, 1f);
+		map.put(TransformationType.TRANSFORM, 0f);
+	}
     public static void main( String [] args ) {
         GLProfile glprofile = GLProfile.getDefault();
         GLCapabilities glcapabilities = new GLCapabilities(glprofile);
         final GLCanvas glcanvas = new GLCanvas( glcapabilities );
-
-        glcanvas.addGLEventListener( new GLEventListener() {
+        initMap();
+        
+        glcanvas.addGLEventListener(new GLEventListener() {
             
             @Override //when window was resized
-            public void reshape(GLAutoDrawable glautodrawable, int x, int y, int width, int height) { 
-            	LaboratoryWorks.setup(glautodrawable.getGL().getGL2(), width, height );
-            }
+            public void reshape(GLAutoDrawable glautodrawable, int x, int y, int width, int height) {}
             
             @Override //work only once in the begining
-            public void init(GLAutoDrawable glautodrawable) {}
+            public void init(GLAutoDrawable glautodrawable) {
+            	GL2 gl2 = glautodrawable.getGL().getGL2();
+                gl2.glMatrixMode(GL2.GL_PROJECTION);  
+                gl2.glLoadIdentity();  
+
+                GLU glu = new GLU(); 
+                glu.gluOrtho2D( 0.0f, 640, 0.0f, 680 );  
+
+                gl2.glMatrixMode( GL2.GL_MODELVIEW );   
+                gl2.glLoadIdentity();
+            }
             
             @Override  //work only once at the end
             public void dispose(GLAutoDrawable glautodrawable) {}
             
             @Override 
             public void display(GLAutoDrawable glautodrawable) {
-            	LaboratoryWorks.render( glautodrawable.getGL().getGL2(), glautodrawable.getWidth(), glautodrawable.getHeight() );
+            	System.out.println("transform " + map.get(TransformationType.TRANSFORM));
+            	System.out.println("scale " + map.get(TransformationType.SCALE));
+            	System.out.println("rotate_c " + map.get(TransformationType.ROTATE_C));
+            	GL2 gl2 = glautodrawable.getGL().getGL2();
+            	gl2.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+            	gl2.glLoadIdentity(); 
+            	
+            	if(map.get(TransformationType.ROTATE_C) != 0f) {
+            		GLU glu = new GLU(); 
+                	gl2.glRotatef(map.get(TransformationType.ROTATE_C), 0f, 0f, 1f);
+            	}
+            	
+            	gl2.glTranslatef(220f, 90, 0f);
+            	gl2.glScalef(map.get(TransformationType.SCALE), map.get(TransformationType.SCALE), 1);
+            	gl2.glTranslatef(map.get(TransformationType.TRANSFORM), map.get(TransformationType.TRANSFORM), 1);
+            	
+            	//gl2.glRotatef(map.get(TransformationType.ROTATE_Z), 0f, 0f, 1f);
+                drawK(gl2, new Random());
+                drawP(gl2, new Random());
             }
         });
 
-        final KeyFrame frame = new KeyFrame("KP");
+        final Frame frame = new Frame("KP");
         frame.add(glcanvas);
         glcanvas.addKeyListener(new KeyListener() {
 			
         	@Override
         	public void keyPressed(KeyEvent e) {
-        		System.out.println("p" + e.getKeyChar() + " " + e.getKeyCode());
+        		switch (e.getKeyCode()) {
+				case 37:
+					map.put(TransformationType.SCALE, map.get(TransformationType.SCALE)+0.01f);
+					glcanvas.display();
+					break;
+				case 38:
+					map.put(TransformationType.TRANSFORM, map.get(TransformationType.TRANSFORM)+1f); 
+					glcanvas.display();
+					break;
+				case 39:
+					map.put(TransformationType.ROTATE_Z, map.get(TransformationType.ROTATE_Z)+1f); 
+					glcanvas.display();
+					break;
+				
+				case 40:
+					map.put(TransformationType.ROTATE_C, map.get(TransformationType.ROTATE_C)+1f); 
+					glcanvas.display();
+					break;
+				default:
+					break;
+				}
         	}
         	@Override
-        	public void keyReleased(KeyEvent e) {
-        		System.out.println("r" + e.getKeyChar() + " " + e.getKeyCode());
-        	}
+        	public void keyReleased(KeyEvent e) {}
         	
         	@Override
         	public void keyTyped(KeyEvent e) {}
 		});
+        
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowevent) {
                 frame.remove(glcanvas);
@@ -68,16 +125,9 @@ public class LaboratoryWorks {
         frame.setVisible(true);
     }
     
-    protected static void render(GL2 gl2, int width, int height ) {
-        gl2.glClear(GL.GL_COLOR_BUFFER_BIT); //TODO: 
-        gl2.glLoadIdentity();		//TODO: 
-        drawK(gl2, new Random());
-        drawP(gl2, new Random());
-    }
-    
     protected static void drawP(GL2 gl2, Random random){
     	Random r = new Random();
-    	gl2.glBegin(GL.GL_LINE_LOOP); //TODO: 
+    	gl2.glBegin(GL.GL_LINE_LOOP); 
     	newPoint(gl2, 270, 0, r);
     	newPoint(gl2, 270, 230, r);
     	newPoint(gl2, 290, 230, r);
@@ -97,7 +147,6 @@ public class LaboratoryWorks {
     	newPoint(gl2, 270, 120, r);
     	gl2.glEnd();
     }
-    
     
     protected static void drawK(GL2 gl2, Random random){
     	Random r = new Random();
@@ -187,18 +236,4 @@ public class LaboratoryWorks {
     	gl2.glColor3f( random.nextFloat(), random.nextFloat(), random.nextFloat());
         gl2.glVertex2f( x, y );
 	}
-
-    
-    protected static void setup( GL2 gl2, int width, int height ) {
-        gl2.glMatrixMode(GL2.GL_PROJECTION ); //TODO: 
-        gl2.glLoadIdentity(); //TODO: 
-
-        GLU glu = new GLU(); //TODO: 
-        glu.gluOrtho2D( 0.0f, width, 0.0f, height ); //TODO: 
-
-        gl2.glMatrixMode( GL2.GL_MODELVIEW );  //TODO: 
-        gl2.glLoadIdentity(); //TODO: 
-
-        gl2.glViewport( 0, 0, width, height ); //TODO: 
-    }
 }
---
+++ src/graphics/TransformationType.java
@@ -0,0 +1,8 @@
+package graphics;
+
+public enum TransformationType {
+	SCALE,
+	TRANSFORM,
+	ROTATE_C, //center
+	ROTATE_Z, //zero
+}
